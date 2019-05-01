769ce162f3cebd6a6932544a67fb0cd5ca358790
Marvin Gaube <dev@marvingaube.de>
2019-05-01 16:55:30 +0200

    Remove build error from test.

--- src/test.ts
+++ src/test.ts
@@ -5,7 +5,7 @@ const data = new Datalib();
 data.set("test", 123);
 data.dump("system");
 data.subscribe("system").on("data", () => {
-  console.log(data.data.system);
+  console.log(data.data);
 });
 
 
